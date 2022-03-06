0c8897918034ca1843fa585b8e048cfa4c3cdb30
Anton Alekseevich <antonboch12.44@gmail.com>
2022-03-06 20:46:03 +0400

    GStreamer handler modification.
    + Creating Bin
    + Creating Context

--- handles/gstreamer/__init__.py
+++ handles/gstreamer/__init__.py
@@ -8,8 +8,15 @@ from gi.repository import Gst
 
 
 class GStreamer:
-    last_id: int = 0
+    # identifiers
+    last_stream_id: int = 0
+    last_context_id: int = 0
+    last_bin_id: int = 0
+
+    # dictionaries
     streams: dict = {}
+    contexts: dict = {}
+    bins: dict = {}
 
     def __init__(self, argv=None):
         if argv is None:
@@ -23,6 +30,24 @@ class GStreamer:
         if not self.isInitialized:
             print("GStreamer not working with this arguments")
 
+    def createNewBin(self, bin_name):
+        binary_data: Gst.Bin
+
+        binary_data = Gst.Bin.new(bin_name)
+
+        self.bins.update({self.last_bin_id: binary_data})
+
+        self.last_bin_id += 1
+
+    def createNewContext(self, ctx_type, isPersistent):
+        context: Gst.Context
+
+        context = Gst.Context.new(ctx_type, isPersistent)
+
+        self.contexts.update({self.last_context_id: context})
+
+        self.last_context_id += 1
+
     def createNewStream(self, sid, caps=None, stream_type=8, stream_flags=2):
         stream: Gst.Stream
 
@@ -33,10 +58,10 @@ class GStreamer:
                 stream_type = Gst.StreamType(stream_type)
             else:
                 print("stream_type MUST in 1, 2, 4, 8, 16. Defaulting to 8.")
-                stream_type = Gst.StreamType(8)
+                stream_type = Gst.StreamType.CONTAINER
         else:
             print(f"stream_type is {type(stream_type)} which is not allowed for.")
-            stream_type = Gst.StreamType(8)
+            stream_type = Gst.StreamType.CONTAINER
 
         if stream_flags is Gst.StreamFlags:
             pass
@@ -45,10 +70,10 @@ class GStreamer:
                 stream_flags = Gst.StreamFlags(stream_flags)
             else:
                 print("stream_flags MUST in 0, 1, 2, 4. Defaulting to 2.")
-                stream_flags = Gst.StreamFlags(2)
+                stream_flags = Gst.StreamFlags.SELECT
         else:
             print(f"stream_flags is {type(stream_flags)} which is not allowed for.")
-            stream_flags = Gst.StreamFlags(2)
+            stream_flags = Gst.StreamFlags.SELECT
 
         new_caps = Gst.Caps.new_empty_simple("container")
 
@@ -61,9 +86,9 @@ class GStreamer:
 
         stream = Gst.Stream(stream_id=sid, caps=new_caps, stream_type=stream_type, stream_flags=stream_flags)
 
-        self.streams.update({sid: (self.last_id, stream)})
+        self.streams.update({sid: (self.last_stream_id, stream)})
 
-        self.last_id += 1
+        self.last_stream_id += 1
 
     def __del__(self):
         Gst.deinit()
